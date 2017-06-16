7a3a2a130a67317fa85215201a246ecb79bca844
mahananda9889 <mahanandaray9889@gmail.com>
2017-06-16 15:24:12 +0600

    class-8

---
+++ home-2_Class-08/assets/js/main.js
@@ -0,0 +1,60 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+        $(".case-studies-carousel").owlCarousel({
+            items: 3,
+            loop: true,
+            nav: false,
+            autoplay: false,
+            dots: true,
+            margin: 30
+        });
+
+
+        $(".testimonial-carousel").owlCarousel({
+            items: 1,
+            loop: true,
+            nav: false,
+            autoplay: false,
+            dots: true,
+        });
+
+
+        $(".logo-carousel").owlCarousel({
+            items: 6,
+            loop: true,
+            nav: false,
+            autoplay: true,
+            dots: false,
+            margin: 30
+        });
+
+
+
+        $(".single-testimonial-item-caroudel").owlCarousel({
+            items: 1,
+            loop: true,
+            nav: false,
+            autoplay: true,
+            dots: false,
+            margin: 0
+        });
+
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
